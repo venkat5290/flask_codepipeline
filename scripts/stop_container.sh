10118810e14290077b3ee1800b3047baa9e5e68d
venkat5290 <injarapu.venkat@gmail.com>
2024-01-29 00:52:17 +0530

    Updated Application Stop script

--- scripts/stop_container.sh
+++ scripts/stop_container.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
-
 set -e 
-
-echo "stopping container"
+echo "stopping and removing running containers"
+containerIds=`docker ps | awk 'NR>1 {print$1}'`
+echo $containerIds
+docker stop $containerIds
+docker rm $containerIds
+docker ps -a
+echo "Container cleanup successfull"
